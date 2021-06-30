f6f2df4673be61938bbc1ad365cc9e6870836ff1
Paule <paul.krueger@alumni.fh-aachen.de>
2021-06-30 14:07:34 +0200

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -15,7 +15,7 @@ do
     find "$D" -name "*.md" -maxdepth 1 -type f | sort -n | while read -r F
     do
         echo "Appending file $F"
-        echo "Fragen aus der Datei [$(basename "$F" .md)](./$(echo "$F" | sed -e 's/(/%28/g' -e 's/)/%29/g'))."  >> Fragenkatalog.md
+        echo "Fragen aus der Datei [$(basename "$F" .md)]($(echo "$F" | sed -e 's/(/%28/g' -e 's/)/%29/g'))."  >> Fragenkatalog.md
         escapedFolder=$(echo "$D" | sed -e 's/(/%28/g' -e 's/)/%29/g')
         cat "$F" | sed -e '0,/#/s/^##*.*/<details><summary>&<\/summary>\n/' \
                 -e 's/^##*.*/<\/details>\n<details><summary>&<\/summary>\n/' \
